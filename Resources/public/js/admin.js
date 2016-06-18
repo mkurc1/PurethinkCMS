508eb8fa17835b834cdc1516c9a1ba5b7c121086
Michał Kurcewicz <mkurc1@gmail.com>
2016-06-18 18:20:17 +0200

    Show only on language tab in form

--- Resources/public/js/admin.js
+++ Resources/public/js/admin.js
@@ -1,16 +1,65 @@
-(function ($) {
-
-    $(function () {
-        hideTranslationTabForOneLanguage();
+(function () {
+    $(document).ready(function () {
+        LangTabs();
     });
 
-    function hideTranslationTabForOneLanguage() {
-        $('ul.a2lix_translationsLocales').each(function() {
-            console.log($(this).children('li').size());
-            if ($(this).children('li').size() < 2) {
-                $(this).hide();
-            }
-        });
-    }
+    function LangTabs() {
+        var $langTabs,
+            $langFields;
 
-})(jQuery);
+        _init();
+
+        function _changeLangNotUsedTabs(event) {
+            var $clickedTab = $(event.currentTarget),
+                clickedTabIndex = $clickedTab.index();
+
+            for (var i = 1; i < $langTabs.length; i++) {
+                var $otherLangTab = $($langTabs[i]);
+
+                $otherLangTab.find('li:eq(' + clickedTabIndex + ') > a').click();
+            }
+        }
+
+        function _hideNotUsedTabs() {
+            for (var i = 1; i < $langTabs.length; i++) {
+                var $langTab = $($langTabs[i]);
+
+                $langTab.hide();
+            }
+        }
+
+        function _isOnlyOneLang() {
+            return $langTabs.first().find('li').length === 1;
+        }
+
+        function _hideAllTabs() {
+            $langTabs.hide();
+        }
+
+        function _events() {
+            var $mainLangTab;
+
+            $mainLangTab = $langTabs.first();
+
+            $mainLangTab.on('click', 'li', function (event) {
+                _changeLangNotUsedTabs(event);
+            });
+        }
+
+        function _init() {
+            $langTabs = $('.a2lix_translationsLocales');
+            $langFields = $('.a2lix_translationsFields');
+
+            if (!$langTabs.length) {
+                return false;
+            }
+
+            if (_isOnlyOneLang()) {
+                _hideAllTabs();
+            } else if ($langTabs.length > 1) {
+                _hideNotUsedTabs();
+                _events();
+            }
+        }
+    }
+})();

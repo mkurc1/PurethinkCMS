745a11305c1551f890fa5b120597268a50e11980
Michał Kurcewicz <mkurc1@gmail.com>
2016-10-12 21:57:58 +0200

    Fix query for get max position

--- Repository/LanguageRepository.php
+++ Repository/LanguageRepository.php
@@ -6,6 +6,14 @@ use Doctrine\ORM\EntityRepository;
 
 class LanguageRepository extends EntityRepository
 {
+    public function getLastPosition()
+    {
+        return (int)$this->createQueryBuilder('a')
+            ->select('MAX(a.position)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
     public function getPublicLanguages()
     {
         $qb = $this->createQueryBuilder('a')
